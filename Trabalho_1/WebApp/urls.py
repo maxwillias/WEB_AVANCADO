3a12c4a41ed003cd724fe29ff6520ccf7c02909c
maxwillias <willias_junior@hotmail.com>
2021-07-19 21:37:49 -0300

    finalização das paginas

--- Trabalho_1/WebApp/urls.py
+++ Trabalho_1/WebApp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import index, html, css, javascript, frontend, backend, w3c
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="index"),
+    path('html', html, name="html"),
+    path('css', css, name="css"),
+    path('javascript', javascript, name="javascript"),
+    path('frontend', frontend, name="frontend"),
+    path('backend', backend, name="backend"),
+    path('w3c', w3c, name="w3c"),
 ]
--- Trabalho_1/WebApp/views.py
+++ Trabalho_1/WebApp/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index (request):
+
+
+def index(request):
     return render(request, 'index.html')
+
+
+def html(request):
+    return render(request, 'html.html')
+
+
+def css(request):
+    return render(request, 'css.html')
+
+
+def javascript(request):
+    return render(request, 'javascript.html')
+
+
+def frontend(request):
+    return render(request, 'frontend.html')
+
+
+def backend(request):
+    return render(request, 'backend.html')
+
+
+def w3c(request):
+    return render(request, 'w3c.html')
+
+
